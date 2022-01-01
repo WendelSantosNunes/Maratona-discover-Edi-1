c40e1461f7e0abe49933c58a4ab3ec7134a60aea
Wendel dos Santos Nunes <wendelnunes9999@gmail.com>
2022-01-01 17:38:36 -0300

    Project update

--- script.js
+++ script.js
@@ -16,31 +16,63 @@ const transactions = [
     id: 1,
     description: 'Luz',
     amount: -50000,
-    date: '23/01/20/21'
+    date: '23/01/2021'
   },
   {
     id: 2,
     description: 'Criação website',
     amount: 500000,
-    date: '23/01/20/21'
+    date: '23/01/2021'
   },
   {
     id: 3,
     description: 'Internet',
     amount: -20000,
-    date: '23/01/20/21'
+    date: '23/01/2021'
   }
 ]
 
 const Transaction = {
+  all: transactions,
+
+  add(transaction) {
+    Transaction.all.push(transaction)
+
+    App.reload()
+  },
+
   incomes() {
-    // Somar as entradas
+    let income = 0
+    // pegar todas as transações
+    // para cada transação
+    Transaction.all.forEach(transaction => {
+      // se ela for maior que zero
+      if (transaction.amount > 0) {
+        // Somar a uma variável e retorna a variável
+        income += transaction.amount
+      }
+    })
+
+    return income
   },
+
   expenses() {
-    // Somar as saídas
+    let expense = 0
+    // pegar todas as transações
+    // para cada transação
+    Transaction.all.forEach(transaction => {
+      // se ela for menor que zero
+      if (transaction.amount < 0) {
+        // Somar a uma variável e retorna a variável
+        expense += transaction.amount
+      }
+    })
+
+    return expense
   },
+
   total() {
-    // Entradas - Saídas
+    return Transaction.incomes() + Transaction.expenses()
   }
 }
 
@@ -66,15 +98,59 @@ const DOM = {
       </td>
     `
     return html
+  },
+  updateBalance() {
+    document.getElementById('incomeDisplay').innerHTML = Utils.formatCurrency(
+      Transaction.incomes()
+    )
+    document.getElementById('expenseDisplay').innerHTML = Utils.formatCurrency(
+      Transaction.expenses()
+    )
+    document.getElementById('totalDisplay').innerHTML = Utils.formatCurrency(
+      Transaction.total()
+    )
+  },
+  clearTransactions() {
+    DOM.transactionsContainer.innerHTML = ''
   }
 }
 
 const Utils = {
   formatCurrency(value) {
     const signal = Number(value) < 0 ? '-' : ''
+
+    value = String(value).replace(/\D/g, '')
+
+    value = Number(value) / 100
+
+    value = value.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    })
+
+    return signal + ' ' + value
   }
 }
 
-transactions.forEach(function (transactions) {
-  DOM.addTransaction(transactions)
+const App = {
+  init() {
+    Transaction.all.forEach(transactions => {
+      DOM.addTransaction(transactions)
+    })
+
+    DOM.updateBalance()
+  },
+  reload() {
+    DOM.clearTransactions()
+    App.init()
+  }
+}
+
+App.init()
+
+Transaction.add({
+  id: 39,
+  description: 'Hello',
+  amount: 200,
+  date: '23/01/2021'
 })
