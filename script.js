d0c5b11080d4338cc3e2c52a32ed062a1312794d
Wendel dos Santos Nunes <wendelnunes9999@gmail.com>
2022-01-01 14:50:39 -0300

    Project update

--- script.js
+++ script.js
@@ -1,8 +1,80 @@
 const Modal = {
-  open() {
-    document.querySelector('.modal-overlay').classList.add('active')
-  },
-  close() {
-    document.querySelector('.modal-overlay').classList.remove('active')
+  open_close() {
+    const classes = document.querySelector('.modal-overlay').classList
+    const result = classes.toggle('active')
+
+    if (result) {
+      document.querySelector('.modal-overlay').classList.add('active')
+    } else {
+      document.querySelector('.modal-overlay').classList.remove('active')
+    }
   }
 }
+
+const transactions = [
+  {
+    id: 1,
+    description: 'Luz',
+    amount: -50000,
+    date: '23/01/20/21'
+  },
+  {
+    id: 2,
+    description: 'Criação website',
+    amount: 500000,
+    date: '23/01/20/21'
+  },
+  {
+    id: 3,
+    description: 'Internet',
+    amount: -20000,
+    date: '23/01/20/21'
+  }
+]
+
+const Transaction = {
+  incomes() {
+    // Somar as entradas
+  },
+  expenses() {
+    // Somar as saídas
+  },
+  total() {
+    // Entradas - Saídas
+  }
+}
+
+const DOM = {
+  transactionsContainer: document.querySelector('#data-table tbody'),
+  addTransaction(transaction, index) {
+    const tr = document.createElement('tr')
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+    DOM.transactionsContainer.appendChild(tr)
+  },
+  innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? 'income' : 'expense'
+
+    const amout = Utils.formatCurrency(transaction.amount)
+
+    const html = `
+      <td class="description">${transaction.description}</td>
+      <td class=${CSSclass}>${amout}</td>
+      <td class="date">${transaction.date}</td>
+      <td>
+        <img src="./assets/minus.svg" alt="Remover transação" />
+      </td>
+    `
+    return html
+  }
+}
+
+const Utils = {
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? '-' : ''
+  }
+}
+
+transactions.forEach(function (transactions) {
+  DOM.addTransaction(transactions)
+})
