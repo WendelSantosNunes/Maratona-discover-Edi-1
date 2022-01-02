21ecd8e4772151ea1da8eff530f2b059dd8bc083
Wendel dos Santos Nunes <wendelnunes9999@gmail.com>
2022-01-02 15:21:47 -0300

    Project update

--- script.js
+++ script.js
@@ -133,8 +133,8 @@ const Utils = {
   },
 
   formatAmount(value) {
-    value = value = Number(value.replace(/\,\./g, '')) * 100
-    return value
+    value = value * 100
+    return Math.round(value)
   },
 
   formatDate(value) {
